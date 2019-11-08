42c3a7a68a887fbf2825be693edad0fd74db596d
thiago alves de oliveira <thiagoadoq@gmail.com>
2019-11-07 21:58:44 -0300

    Subindo projeto

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: '1323',
+  database: 'sqlnode',
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+};
